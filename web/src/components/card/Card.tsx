597e4602ba9c3d4efd829c664e714f8367ec1345
rickrribeiro <rickribeiro99@gmail.com>
2022-12-06 08:33:16 -0300

    removing price and add explore city button to recommendation cart

--- web/src/components/card/Card.tsx
+++ web/src/components/card/Card.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Card.css'
+import { Button } from '@material-ui/core'
+import { useNavigate } from "react-router-dom";
 
 interface RecommendationProps {
     id: string;
@@ -7,16 +9,23 @@ interface RecommendationProps {
     src : string;
     description: string;
     price?: number;
+    city:{
+        name: string;
+    }
   }
 
 function Card(props: RecommendationProps) {
+
+    let navigate = useNavigate(); 
+
     return (
         <div className='card'>
             <img src={props.src} alt="" />
             <div className="card__info">
                 <h2>{props.title}</h2>
                 <h4>{props.description}</h4>
-                <h3>£{props.price}</h3>{/* TODO FIXAR NO CANTO INFERIOR DEPOIS */}
+               
+                <Button onClick={() =>navigate(`/city/${props.city.name}`)} variant='outlined'>Explore city</Button>
   
             </div>
         </div>
