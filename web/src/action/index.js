0a2475de4b998859e929121a8abaa3729d8ddb47
rickrribeiro <rickribeiro99@gmail.com>
2022-12-07 19:53:43 -0300

    fix create article and show created articles

--- web/src/action/index.js
+++ web/src/action/index.js
@@ -1,169 +1,191 @@
 import db, { auth, provider, storage } from "../firebase";
 import { SET_LOADING_STATUS, SET_USER, GET_ARTICLES } from "./actionType";
-import { collection, doc, setDoc, addDoc } from "firebase/firestore"; 
+import { collection, doc, setDoc, addDoc, getDocs } from "firebase/firestore";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
 export function setUser(payload) {
-
-	return {
-		type: SET_USER,
-		user: payload,
-	};
+  return {
+    type: SET_USER,
+    user: payload,
+  };
 }
 
 export function setLoading(status) {
-	return {
-		type: SET_LOADING_STATUS,
-		status: status,
-	};
+  return {
+    type: SET_LOADING_STATUS,
+    status: status,
+  };
 }
 
 export function getArticles(payload, id) {
-	return {
-		type: GET_ARTICLES,
-		payload: payload,
-		id: id,
-	};
+  return {
+    type: GET_ARTICLES,
+    payload: payload,
+    id: id,
+  };
 }
 
 export function getUserAuth() {
-	return (dispatch) => {
-		auth.onAuthStateChanged(async (user) => {
-			if (user) {
-				dispatch(setUser(user));
-			}
-		});
-	};
+  return (dispatch) => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        dispatch(setUser(user));
+      }
+    });
+  };
 }
 
 export function signInAPI() {
-	return (dispatch) => {
-
-		console.log("dispatch")
-		signInWithPopup(auth, provider)
-		.then((result) => {
-		
-			const credential = GoogleAuthProvider.credentialFromResult(result);
-			const token = credential.accessToken;
-			console.log(result.user)
-			dispatch(setUser(result.user));
-		}).catch((error) => {
-			console.log(error.message)
-			alert(error.message);
-		});
-
-	};
+  return (dispatch) => {
+    console.log("dispatch");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        console.log(result.user);
+        dispatch(setUser(result.user));
+      })
+      .catch((error) => {
+        console.log(error.message);
+        alert(error.message);
+      });
+  };
 }
 
 export function signOutAPI() {
-	return (dispatch) => {
-		auth.signOut()
-			.then(() => dispatch(setUser(null)))
-			.catch((err) => alert(err.message));
-	};
+  return (dispatch) => {
+    auth
+      .signOut()
+      .then(() => dispatch(setUser(null)))
+      .catch((err) => alert(err.message));
+  };
 }
 
 export function postArticleAPI(payload) {
-	console.log(payload)
-	return (dispatch) => {
-		if (payload.image !== "") {
-			dispatch(setLoading(true));
-			const upload = storage.ref(`images/${payload.image.name}`).put(payload.image);
-			upload.on(
-				"state_changed",
-				(snapshot) => {
-					const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-				},
-				(err) => alert(err),
-				async () => {
-					const downloadURL = await upload.snapshot.ref.getDownloadURL();
-					addDoc(collection(db,"articles"),{
-						actor: {
-							// TODO: PEGAR USER ID DPS QUE TIVER LOGANDO
-							id: payload.user.email,
-							title:payload.user.displayName,
-							date: '07/12/2022',
-							image:payload.user.photoURL,
-						},
-						video: payload.video,
-						sharedImg: downloadURL,
-						likes: {
-							count: 0,
-							whoLiked: [],
-						},
-						comments: 0,
-						description: payload.description,
-						contact: payload.contact
-					});
-					dispatch(setLoading(false));
-				}
-			);
-		} else if (payload.video) {
-			dispatch(setLoading(true));
-			addDoc(collection(db,"articles"),{
-				actor: {
-					// TODO: PEGAR USER ID DPS QUE TIVER LOGANDO
-					id: payload.user.email,
-					title:payload.user.displayName,
-					date: '07/12/2022',
-					image:payload.user.photoURL,
-				},
-				video: payload.video,
-				sharedImg: "",
-				likes: {
-					count: 0,
-					whoLiked: [],
-				},
-				comments: 0,
-				description: payload.description,
-				contact: payload.contact
-			});
-			dispatch(setLoading(false));
-		} else if (payload.image === "" && payload.video === "") {
-			dispatch(setLoading(true));
-			addDoc(collection(db,"articles"), {
-				actor: {
-					// TODO: PEGAR USER ID DPS QUE TIVER LOGANDO
-					id: payload.user.email,
-					title:payload.user.displayName,
-					date: '07/12/2022',
-					image:payload.user.photoURL,
-				},
-				video: "",
-				sharedImg: "",
-				likes: {
-					count: 0,
-					whoLiked: [],
-				},
-				comments: 0,
-				description: payload.description,
-				contact: payload.contact
-			});
-			dispatch(setLoading(false));
-		}
-	};
+  console.log(payload);
+  return (dispatch) => {
+    if (payload.image !== "") {
+      dispatch(setLoading(true));
+      const upload = storage
+        .ref(`images/${payload.image.name}`)
+        .put(payload.image);
+      upload.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (err) => alert(err),
+        async () => {
+          const downloadURL = await upload.snapshot.ref.getDownloadURL();
+          addDoc(collection(db, "articles"), {
+            actor: {
+              // TODO: PEGAR USER ID DPS QUE TIVER LOGANDO
+              id: payload.user.email,
+              title: payload.user.displayName,
+              date: "07/12/2022",
+              image: payload.user.photoURL,
+            },
+            video: payload.video,
+            sharedImg: downloadURL,
+            likes: {
+              count: 0,
+              whoLiked: [],
+            },
+            comments: 0,
+            description: payload.description,
+            contact: payload.contact,
+          });
+          dispatch(setLoading(false));
+        }
+      );
+    } else if (payload.video) {
+      dispatch(setLoading(true));
+      addDoc(collection(db, "articles"), {
+        actor: {
+          // TODO: PEGAR USER ID DPS QUE TIVER LOGANDO
+          id: payload.user.email,
+          title: payload.user.displayName,
+          date: "07/12/2022",
+          image: payload.user.photoURL,
+        },
+        video: payload.video,
+        sharedImg: "",
+        likes: {
+          count: 0,
+          whoLiked: [],
+        },
+        comments: 0,
+        description: payload.description,
+        contact: payload.contact,
+      });
+      dispatch(setLoading(false));
+    } else if (payload.image === "" && payload.video === "") {
+      dispatch(setLoading(true));
+      addDoc(collection(db, "articles"), {
+        actor: {
+          // TODO: PEGAR USER ID DPS QUE TIVER LOGANDO
+          id: payload.user.email,
+          title: payload.user.displayName,
+          date: "07/12/2022",
+          image: payload.user.photoURL,
+        },
+        video: "",
+        sharedImg: "",
+        likes: {
+          count: 0,
+          whoLiked: [],
+        },
+        comments: 0,
+        description: payload.description,
+        contact: payload.contact,
+      });
+      dispatch(setLoading(false));
+    }
+  };
 }
 
 export function getArticlesAPI() {
-	return (dispatch) => {
-		dispatch(setLoading(true));
-		let payload;
-		let id;
-		collection(db,"articles")
-		// TODO -> VER ISSO AQUI QUE N TA INDO
-			// .orderBy("actor.date", "desc")
-			// .onSnapshot((snapshot) => {
-			// 	payload = snapshot.docs.map((doc) => doc.data());
-			// 	id = snapshot.docs.map((doc) => doc.id);
-			// 	dispatch(getArticles(payload, id));
-			// });
-		dispatch(setLoading(false));
-	};
+  console.log("GETEeeeeeeeeE");
+  return (dispatch) => {
+    dispatch(setLoading(true));
+    let payload;
+    let id;
+    collection(db, "articles")
+      .orderBy("actor.date", "desc")
+      .onSnapshot((snapshot) => {
+        payload = snapshot.docs.map((doc) => doc.data());
+        id = snapshot.docs.map((doc) => doc.id);
+        dispatch(getArticles(payload, id));
+      });
+    dispatch(setLoading(false));
+  };
+}
+
+export function getArticlesAPI2() {
+  console.log("GETETETETETETE3");
+  dispatch(setLoading(true));
+  return (dispatch) => {
+    let payload;
+    let id;
+    console.log("11111111");
+    getDocs(collection(db, "articles")).then((snapshot) => {
+      console.log("aq");
+      snapshot.docs.map((doc) => {
+        payload = snapshot.docs.map((doc) => doc.data());
+        id = snapshot.docs.map((doc) => doc.id);
+        console.log(doc.data());
+        console.log("22222");
+        dispatch(getArticles(payload, id));
+      });
+    });
+	dispatch(setLoading(false));
+  };
 }
 
 export function updateArticleAPI(payload) {
-	return (dispatch) => {
-		collection(db,"articles").doc(payload.id).update(payload.update);
-	};
+  return (dispatch) => {
+    collection(db, "articles").doc(payload.id).update(payload.update);
+  };
 }
--- web/src/components/city/main/Main.jsx
+++ web/src/components/city/main/Main.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import ReactPlayer from "react-player";
 
 import styled from "styled-components";
-import { getArticlesAPI, updateArticleAPI } from "../../../action";
+import { getArticlesAPI,getArticlesAPI2, updateArticleAPI } from "../../../action";
 import PostalModal from "../postalmodal/PostalModal";
 
 const Container = styled.div`
@@ -207,6 +207,7 @@ function Main(props) {
 	const [showModal, setShowModal] = useState("close");
 
 	useEffect(() => {
+		console.log("q porra")
 		props.getArticles();
 	}, []);
 
@@ -227,6 +228,9 @@ function Main(props) {
 				break;
 		}
 	};
+	console.log("PROPPPPPPPPPPP")
+
+	console.log(props)
 
 	let articles = [
 		{
@@ -343,7 +347,61 @@ function Main(props) {
 				{/* false aq em baixo, TODO qnd tiver back */}
 				{/* { props.articles.length > 0 &&
 					props.articles.map((article, key) => ( */}
-				{articles.map((article, key) => (
+				{ props.articles.length > 0 && props.articles.map((article, key) => (
+					<Article key={key}>
+						<SharedActor>
+							<a>
+								{article.actor.image ? <img src={article.actor.image} alt="" /> : <img src="/images/user.svg" alt="" />}
+								<div>
+									<span>{article.actor.title}</span>
+									<span>{article.actor.description}</span>
+									<span>{article.actor.date}</span>{/* <span>{article.actor.date.toDate().toLocaleDateString()}</span> */}
+								</div>
+							</a>
+							<button>
+								<img src="/images/ellipses.svg" alt="" />
+							</button>
+						</SharedActor>
+						<Description>{article.description}</Description>
+						<SharedImage>
+							<a>{!article.sharedImg && article.video ? <ReactPlayer width={"100%"} url={article.video} /> : article.sharedImg && <img src={article.sharedImg} alt="" />}</a>
+						</SharedImage>
+						<SocialCount>
+							{article.likes.count > 0 && (
+								<>
+									<li>
+										<button>
+											<img src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb" alt="" />
+											{/* <img src="https://static-exp1.licdn.com/sc/h/7fx9nkd7mx8avdpqm5hqcbi97" alt="" /> */}
+											<span>{article.likes.count}</span>
+										</button>
+									</li>
+									<li>
+										<a>{article.comments} comments (currently not working)</a>
+									</li>
+								</>
+							)}
+						</SocialCount>
+						<SocialActions>
+							{/* <button onClick={(event) => likeHandler(event, key, props.ids[key])} className={article.likes.whoLiked.indexOf(props.user.email) >= 0 ? "active" : null}> TODO QUANDO TIVER AUTH*/}
+							<button onClick={(event) => likeHandler(event, key, props.ids[key])} className={article.likes.whoLiked.indexOf('EMAIL@EMAIL') >= 0 ? "active" : null}>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="rgba(0, 0, 0, 0.6)" width="24" height="24" focusable="false">
+									<path d="M19.46 11l-3.91-3.91a7 7 0 01-1.69-2.74l-.49-1.47A2.76 2.76 0 0010.76 1 2.75 2.75 0 008 3.74v1.12a9.19 9.19 0 00.46 2.85L8.89 9H4.12A2.12 2.12 0 002 11.12a2.16 2.16 0 00.92 1.76A2.11 2.11 0 002 14.62a2.14 2.14 0 001.28 2 2 2 0 00-.28 1 2.12 2.12 0 002 2.12v.14A2.12 2.12 0 007.12 22h7.49a8.08 8.08 0 003.58-.84l.31-.16H21V11zM19 19h-1l-.73.37a6.14 6.14 0 01-2.69.63H7.72a1 1 0 01-1-.72l-.25-.87-.85-.41A1 1 0 015 17l.17-1-.76-.74A1 1 0 014.27 14l.66-1.09-.73-1.1a.49.49 0 01.08-.7.48.48 0 01.34-.11h7.05l-1.31-3.92A7 7 0 0110 4.86V3.75a.77.77 0 01.75-.75.75.75 0 01.71.51L12 5a9 9 0 002.13 3.5l4.5 4.5H19z"></path>
+								</svg>
+								<span>Like</span>
+							</button>
+							<button>
+								<img src="/images/comment-icon.svg" alt="" />
+								<span>Comment</span>
+							</button>
+							<button>
+								<img src="/images/send-icon.svg" alt="" />
+								<span>Get Contact</span>
+							</button>
+						</SocialActions>
+					</Article>
+				))}
+				{ articles.map((article, key) => (
 					<Article key={key}>
 						<SharedActor>
 							<a>
@@ -413,7 +471,7 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-	getArticles: () => dispatch(getArticlesAPI()),
+	getArticles: () => dispatch(getArticlesAPI2()),
 	likeHandler: (payload) => dispatch(updateArticleAPI(payload)),
 });
 
